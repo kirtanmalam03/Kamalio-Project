0d946315dbfb1b0fc4305b2c7c87591f9a497397
kirtan malam <kirtanmalam1@gmail.com>
2026-08-06 07:37:17 +0530

    Update watch-calls.ps1 to remove log comment
    Removed comment about filtering Kamailio logs.

--- scripts/watch-calls.ps1
+++ scripts/watch-calls.ps1
@@ -1,3 +1,2 @@
-# Tail Kamailio logs filtered to REGISTER / INVITE / BYE / Lookup
 
 docker compose logs -f kamailio 2>&1 | Select-String -Pattern "REGISTER|INVITE|BYE|Lookup|Reply|Failure|REGISTER ok"
