c7ba1494104ae9245bec621460d01c9719015c5e
kirtan malam <kirtanmalam1@gmail.com>
2026-08-06 08:27:53 +0530

    Update show-registrations.ps1

--- scripts/show-registrations.ps1
+++ scripts/show-registrations.ps1
@@ -1,7 +1,7 @@
 
 Write-Host "=== kamcmd ul.dump ==="
 docker compose exec kamailio kamcmd ul.dump
-
+ 
 Write-Host ""
 Write-Host "=== MySQL location ==="
 docker compose exec mysql mysql -ukamailio -pkamailiorw_change_me kamailio -e "SELECT username, contact, received, expires, user_agent FROM location;"
